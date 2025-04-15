e78b1ef4c53950ae96af5d4f973abe0e6d4975d0
olgasielewiesiuk <olga.sielewiesiuk@gmail.com>
2025-04-15 20:57:10 +0200

    Add files via upload

---
+++ prediction.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/v1.0/predict', methods=['GET'])
+def predict():
+    try:
+        a = float(request.args.get('a', 0))
+    except ValueError:
+        a = 0
+
+    # decyzja
+    result = 1 if (a + b) > 5.8 else 0
+
+    # JSON
+    return jsonify({
+        "prediction": result,
+        "features": {"a": a, "b": b}
+    })
+
+if __name__ == '__main__':
+    app.run()
